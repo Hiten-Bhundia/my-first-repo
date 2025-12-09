2beb1ffea605ad38a66b82e3fb0d982ae48a0141
Hiten-Bhundia <hitzy347@gmail.com>
2025-12-09 17:06:46 +0000

    added gradient to circles

--- sketch.js
+++ sketch.js
@@ -6,20 +6,28 @@ function setup() {
 }
 
 function draw() {
-    background(220, 0, 0);
+    background(200);
     textSize(20);
     fill(0);
     text("Github Collaborative Data Drawing Task", 50, 50);
 
     //draw 2 circles
     //change colours of the circle to navy blues with no strokes
+    // Add two squares with two more gradients
     
     noStroke();
     fill(0, 0, 128);
-    circle(400,300,50);
-    circle(400,500,100);
+    radialGradientCircle(400, 300, 50, color(255, 0, 0), color(0, 0, 255));
+    radialGradientCircle(400, 500, 100, color(0, 0, 255), color(255, 0, 0));
 
-    //replace one of the circles with a gradient (colour of your choosing)
 }
 
-
+function radialGradientCircle(x, y, d, innerCol, outerCol) {
+  for (let r = d; r > 0; r--) {
+    let amt = r / d;
+    let c = lerpColor(innerCol, outerCol, amt);
+    fill(c);
+    circle(x, y, r);
+  
+  }
+}
